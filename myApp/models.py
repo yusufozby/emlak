cdf025cd6eac9e1a611c97d785e05fb1f5f2ff30
İbrahim Yunus <iyunusk58@gmail.com>
2023-03-01 17:48:44 +0300

    second commit

--- myApp/models.py
+++ myApp/models.py
@@ -4,19 +4,23 @@ from django.db import models
 
 class Yeni(models.Model):
     title = models.CharField(max_length = 100, verbose_name = "Başlık")
-    text_info = models.CharField(max_length = 1000, verbose_name = "Açıklama")
+    text_info = models.TextField(max_length = 1000, verbose_name = "Açıklama")
     text = models.TextField(max_length=5000, verbose_name = "Text")  
-    yazar = models.CharField(max_length = 100)
-    image = models.FileField(upload_to='',verbose_name='Fotoğraf',blank=True,null=True)
-    image_2 = models.FileField(upload_to='',verbose_name='Fotoğraf',blank=True,null=True)
-    image_3 = models.FileField(upload_to='',verbose_name='Fotoğraf',blank=True,null=True)
-    image_4 = models.FileField(upload_to='',verbose_name='Fotoğraf',blank=True,null=True)
-    image_5 = models.FileField(upload_to='',verbose_name='Fotoğraf',blank=True,null=True)
-    # image_6 = models.FileField(upload_to='',verbose_name='Fotoğraf',blank=True,null=True)
-    owner = models.CharField(max_length = 100,verbose_name="Ev Shibi",blank=True,null=True)
+    advisor = models.CharField(max_length = 100, verbose_name= "Danışman")
+    date = models.DateTimeField(("Tarih"), auto_now_add=True,blank=True,null=True)
+    image = models.FileField(upload_to='',verbose_name='Fotoğraf 1',blank=True,null=True)
+    image_2 = models.FileField(upload_to='',verbose_name='Fotoğraf 2',blank=True,null=True)
+    image_3 = models.FileField(upload_to='',verbose_name='Fotoğraf 3',blank=True,null=True)
+    image_4 = models.FileField(upload_to='',verbose_name='Fotoğraf 4',blank=True,null=True)
+    image_5 = models.FileField(upload_to='',verbose_name='Fotoğraf 5',blank=True,null=True)
+    bd_image = models.FileField(upload_to='',verbose_name='Bd Fotoğraf',blank=True,null=True)
+    owner = models.CharField(max_length = 100,verbose_name="Ev Sahibi",blank=True,null=True)
     fiyat = models.IntegerField(verbose_name="Fiyatı",blank=True,null=True)
+    feature = models.CharField(max_length=100 , verbose_name= "Özellik")
+    
+
 
     def __str__(self):
         show = 'Başlık: ' + self.title
-        show += ', Yazar: ' + self.yazar
+        show += ', Danışman: ' + self.advisor
         return show
--- myApp/views.py
+++ myApp/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render,get_object_or_404
 from myApp.models import *
+from django.db.models import Q
 
 # Create your views here.
 def index(request):
-    post = Yeni.objects.all()
+    post = Yeni.objects.all().order_by("-date")
+
+    # Search Bölümü Kodlaması
+    search = request.GET.get("q")
+    if search:
+        post = post.filter(Q(title__icontains = search) |
+                           Q(feature__icontains = search))
+
     context = {
         'post':post,
     }
